7ebd96032f7f0094e4c393e114d7765750308287
seheichenko-anna <seheichenko.anna@gmail.com>
2024-02-21 20:44:09 +0200

    Update

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { ButtonLoadMore } from './Button.styled';
 
-function Button({ onClick }) {
+const Button = ({ onClick }) => {
   return <ButtonLoadMore onClick={onClick}>Load more</ButtonLoadMore>;
-}
+};
 
 export default Button;
